9189f593d5f4f05d51c402f2e01b22978c1e9baa
nikole <nicolericaldigmail.com>
2016-12-14 17:09:51 -0400

    qr

---
+++ app/src/main/java/com/alumina/nikol/eul/MainActivity.java
@@ -0,0 +1,63 @@
+package com.alumina.nikol.eul;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+    private Button scanBtn;
+    private TextView formatTxt, contentTxt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Se Instancia el botón de Scan
+        scanBtn = (Button)findViewById(R.id.scan);
+        //Se Instancia el Campo de Texto para el nombre del formato de código de barra
+        formatTxt = (TextView)findViewById(R.id.scan_format);
+        //Se Instancia el Campo de Texto para el contenido  del código de barra
+        contentTxt = (TextView)findViewById(R.id.scan_content);
+        //Se agrega la clase MainActivity.java como Listener del evento click del botón de Scan
+        scanBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        //Se responde al evento click
+        if(v.getId()==R.id.scan){
+            //Se instancia un objeto de la clase IntentIntegrator
+            IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+            //Se procede con el proceso de scaneo
+            scanIntegrator.initiateScan();
+        }
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        //Se obtiene el resultado del proceso de scaneo y se parsea
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanningResult != null) {
+            //Quiere decir que se obtuvo resultado pro lo tanto:
+            //Desplegamos en pantalla el contenido del código de barra scaneado
+            String scanContent = scanningResult.getContents();
+            contentTxt.setText("Contenido: " + scanContent);
+            //Desplegamos en pantalla el nombre del formato del código de barra scaneado
+            String scanFormat = scanningResult.getFormatName();
+            formatTxt.setText("Formato: " + scanFormat);
+        }else{
+            //Quiere decir que NO se obtuvo resultado
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "No se ha recibido datos del scaneo!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+}
